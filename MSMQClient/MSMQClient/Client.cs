cf0b947376339cb418101cd9e3d5d1161450dbd5
Artem <asbudin@edu.hse.ru>
2023-12-19 15:36:08 +0500

    инициализация проекта

---
+++ MSMQClient/MSMQClient/Client.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Messaging;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MSMQ
+{
+    public partial class frmMain : Form
+    {
+        private MessageQueue q = null;      // очередь сообщений, в которую будет производиться запись сообщений
+
+        private MessageQueue receiveQueue = null;
+        private Thread t = null;
+        private bool _continue = true;
+        private string Login { get; set; }
+
+
+        // конструктор формы
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (MessageQueue.Exists(tbPath.Text))
+            {
+                // если очередь, путь к которой указан в поле tbPath существует, то открываем ее
+                q = new MessageQueue(tbPath.Text);
+
+                if (!string.IsNullOrEmpty(tbLogin.Text))
+                {
+                    string path = Dns.GetHostName() + $"\\private$\\{tbLogin.Text}";    // путь к очереди сообщений, Dns.GetHostName() - метод, возвращающий имя текущей 
+                    if (!MessageQueue.Exists(path))
+                    {
+
+                        receiveQueue = MessageQueue.Create(path);
+                        receiveQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(String) });
+
+                        Login = tbLogin.Text;
+                        btnSend.Enabled = true;
+                        btnConnect.Enabled = false;
+
+                        q.Send(Login, "system_connect");
+                        // создание потока, отвечающего за работу с очередью сообщений
+                        t = new Thread(ReceiveMessage);
+                        t.Start();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Такой пользователь уже есть в чате");
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Указан неверный путь к очереди, либо очередь не существует");
+        }
+
+        private void ReceiveMessage()
+        {
+            if (receiveQueue == null)
+                return;
+
+            System.Messaging.Message msg = null;
+
+            // входим в бесконечный цикл работы с очередью сообщений
+            while (_continue)
+            {
+                if (receiveQueue.Peek() != null)   // если в очереди есть сообщение, выполняем его чтение, интервал до следующей попытки чтения равен 10 секундам
+                    msg = receiveQueue.Receive(TimeSpan.FromSeconds(10.0));
+
+                rtbMessages.Invoke((MethodInvoker)delegate
+                {
+                    if (msg != null)
+                        rtbMessages.Text += "\n >> " + msg.Label + " : " + msg.Body;     // выводим полученное сообщение на форму
+                });
+                Thread.Sleep(500);          // приостанавливаем работу потока перед тем, как приcтупить к обслуживанию очередного клиента
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // выполняем отправку сообщения в очередь
+            q.Send(tbMessage.Text, Login);
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _continue = false;      // сообщаем, что работа с очередью сообщений завершена
+
+            if (t != null)
+            {
+                t.Abort();          // завершаем поток
+            }
+
+            if (receiveQueue != null)
+            {
+                MessageQueue.Delete(q.Path);      // в случае необходимости удаляем очередь сообщений
+            }
+        }
+    }
+}
---
+++ MSMQServer/MSMQServer/Server.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Messaging;
+using System.Collections;
+
+namespace MSMQ
+{
+    public partial class frmMain : Form
+    {
+        private MessageQueue q = null;          // очередь сообщений
+        private Thread t = null;                // поток, отвечающий за работу с очередью сообщений
+        private bool _continue = true;          // флаг, указывающий продолжается ли работа с мэйлслотом
+
+        private List<string> users = new List<string>();
+
+
+        // конструктор формы
+        public frmMain()
+        {
+            InitializeComponent();
+            string path = Dns.GetHostName() + "\\private$\\ServerQueue";    // путь к очереди сообщений, Dns.GetHostName() - метод, возвращающий имя текущей машины
+
+            // если очередь сообщений с указанным путем существует, то открываем ее, иначе создаем новую
+            if (MessageQueue.Exists(path))
+                q = new MessageQueue(path);
+            else
+                q = MessageQueue.Create(path);
+
+            // задаем форматтер сообщений в очереди
+            q.Formatter = new XmlMessageFormatter(new Type[] { typeof(String) });
+
+            // вывод пути к очереди сообщений в заголовок формы, чтобы можно было его использовать для ввода имени в форме клиента, запущенного на другом вычислительном узле
+            this.Text += "     " + q.Path;
+
+            // создание потока, отвечающего за работу с очередью сообщений
+            Thread t = new Thread(ReceiveMessage);
+            t.Start();
+        }
+
+        // получение сообщения
+        private void ReceiveMessage()
+        {
+            if (q == null)
+                return;
+
+            System.Messaging.Message msg = null;
+
+            // входим в бесконечный цикл работы с очередью сообщений
+            while (_continue)
+            {
+                if (q.Peek() != null)   // если в очереди есть сообщение, выполняем его чтение, интервал до следующей попытки чтения равен 10 секундам
+                    msg = q.Receive(TimeSpan.FromSeconds(10.0));
+                if(msg.Label == "system_connect")
+                {
+                    users.Add(msg.Body.ToString());
+                    System.Messaging.Message msgToUSers = new System.Messaging.Message();
+                    SendMessageToUsers($"Пользователь {msg.Body} зашел в чат!", "message");
+                }
+                else
+                {
+                    SendMessageToUsers(msg.Body.ToString(), msg.Label);
+                }
+
+                rtbMessages.Invoke((MethodInvoker)delegate
+                {
+                    if (msg != null)
+                        rtbMessages.Text += "\n >> " + msg.Label + " : " + msg.Body;     // выводим полученное сообщение на форму
+                });
+
+                Thread.Sleep(500);          // приостанавливаем работу потока перед тем, как приcтупить к обслуживанию очередного клиента
+            }
+        }
+
+        private void SendMessageToUsers(string body, string label)
+        {
+            List<string> connectedUsers = new List<string>();
+            foreach (var user in users)
+            {
+                string path = Dns.GetHostName() + $"\\private$\\{user}";    
+
+                if (MessageQueue.Exists(path))
+                    connectedUsers.Add(user);
+            }
+
+            foreach (var user in connectedUsers)
+            {
+                string path = Dns.GetHostName() + $"\\private$\\{user}";    // путь к очереди сообщений, Dns.GetHostName() - метод, возвращающий имя текущей машины
+
+                MessageQueue queue;
+                // если очередь сообщений с указанным путем существует, то открываем ее, иначе создаем новую
+                if (MessageQueue.Exists(path))
+                    queue = new MessageQueue(path);
+                else
+                    queue = MessageQueue.Create(path);
+
+                queue.Send(body, label);
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _continue = false;      // сообщаем, что работа с очередью сообщений завершена
+
+            if (t != null)
+            {
+                t.Abort();          // завершаем поток
+            }
+
+            if (q != null)
+            {
+                MessageQueue.Delete(q.Path);      // в случае необходимости удаляем очередь сообщений
+            }
+        }
+    }
+}
